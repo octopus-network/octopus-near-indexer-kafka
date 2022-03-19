c840f0bf6f916c8818657dbea3d019a094eef71b
Zhang WenChao <zhangwenchao001@gmail.com>
2022-03-19 09:32:32 +0800

    Init and add generate bin

---
+++ src/bin/generate.rs
@@ -0,0 +1,127 @@
+use clap::Parser;
+use tracing_subscriber::EnvFilter;
+
+/// Octopus Near Indexer Kafka
+/// Watches for stream of blocks from the chain and puts it in Kafka
+#[derive(Parser, Clone, Debug)]
+#[clap(
+version,
+author,
+about,
+setting(clap::AppSettings::DisableHelpSubcommand),
+setting(clap::AppSettings::PropagateVersion),
+setting(clap::AppSettings::NextLineHelp)
+)]
+pub struct Opts {
+    /// Sets a custom config dir. Defaults to ~/.near/
+    #[clap(short, long)]
+    pub home: Option<std::path::PathBuf>,
+    #[clap(subcommand)]
+    pub sub_cmd: SubCommand,
+}
+
+#[derive(Parser, Clone, Debug)]
+pub enum SubCommand {
+    /// Initialize necessary configs
+    Init(InitConfigArgs),
+}
+
+#[derive(Parser, Clone, Debug)]
+pub struct InitConfigArgs {
+    /// chain/network id (localnet, testnet, devnet, betanet)
+    #[clap(short, long)]
+    pub chain_id: Option<String>,
+    /// Account ID for the validator key
+    #[clap(long)]
+    pub account_id: Option<String>,
+    /// Specify private key generated from seed (TESTING ONLY)
+    #[clap(long)]
+    pub test_seed: Option<String>,
+    /// Number of shards to initialize the chain with
+    #[clap(short, long, default_value = "1")]
+    pub num_shards: u64,
+    /// Makes block production fast (TESTING ONLY)
+    #[clap(short, long)]
+    pub fast: bool,
+    /// Genesis file to use when initialize testnet (including downloading)
+    #[clap(short, long)]
+    pub genesis: Option<String>,
+    #[clap(short, long)]
+    /// Download the verified NEAR config file automatically.
+    #[clap(long)]
+    pub download_config: bool,
+    #[clap(long)]
+    pub download_config_url: Option<String>,
+    /// Download the verified NEAR genesis file automatically.
+    #[clap(long)]
+    pub download_genesis: bool,
+    /// Specify a custom download URL for the genesis-file.
+    #[clap(long)]
+    pub download_genesis_url: Option<String>,
+    /// Customize max_gas_burnt_view runtime limit.  If not specified, value
+    /// from genesis configuration will be taken.
+    #[clap(long)]
+    pub max_gas_burnt_view: Option<u64>,
+    /// Initialize boots nodes in <node_key>@<ip_addr> format seperated by commas
+    /// to bootstrap the network and store them in config.json
+    #[clap(long)]
+    pub boot_nodes: Option<String>,
+}
+
+fn main() {
+    // We use it to automatically search the for root certificates to perform HTTPS calls
+    // (sending telemetry and downloading genesis)
+    openssl_probe::init_ssl_cert_env_vars();
+    init_tracing();
+
+    let opts: Opts = Opts::parse();
+
+    let home_dir = opts.home.unwrap_or_else(near_indexer::get_default_home);
+
+    match opts.sub_cmd {
+        SubCommand::Init(config) => near_indexer::init_configs(
+            &home_dir,
+            config.chain_id.as_ref().map(AsRef::as_ref),
+            config.account_id.map(|account_id_string| {
+                near_indexer::near_primitives::types::AccountId::try_from(account_id_string)
+                    .expect("Received accound_id is not valid")
+            }),
+            config.test_seed.as_ref().map(AsRef::as_ref),
+            config.num_shards,
+            config.fast,
+            config.genesis.as_ref().map(AsRef::as_ref),
+            config.download_genesis,
+            config.download_genesis_url.as_ref().map(AsRef::as_ref),
+            config.download_config,
+            config.download_config_url.as_ref().map(AsRef::as_ref),
+            config.boot_nodes.as_ref().map(AsRef::as_ref),
+            config.max_gas_burnt_view,
+        )
+            .expect("Failed to initialize the node config files"),
+    }
+}
+
+fn init_tracing() {
+    let mut env_filter = EnvFilter::new(
+        "tokio_reactor=info,near=info,stats=info,telemetry=info,indexer=info,near_lake=info,aggregated=info",
+    );
+
+    if let Ok(rust_log) = std::env::var("RUST_LOG") {
+        if !rust_log.is_empty() {
+            for directive in rust_log.split(',').filter_map(|s| match s.parse() {
+                Ok(directive) => Some(directive),
+                Err(err) => {
+                    eprintln!("Ignoring directive `{}`: {}", s, err);
+                    None
+                }
+            }) {
+                env_filter = env_filter.add_directive(directive);
+            }
+        }
+    }
+
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_env_filter(env_filter)
+        .with_writer(std::io::stderr)
+        .init();
+}
